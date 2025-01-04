059945110112c2bafab7cba48bf8d76a8d968068
arbiter <arbiter08@naver.com>
2025-01-05 03:18:25 +0900

    #7

--- ing_problem.py
+++ ing_problem.py
@@ -1,4 +1,25 @@
-# 6 실패율
-def solution_6(N, stage):
-  ret = []
-  return ret
+# 7 방문길이
+keyset = { 'U' : (0,-1), 'D' : (0,1), 'L' : (-1,0), 'R' : (1,0) }
+x,y = 5,5
+
+def solution_7(dirs):
+  ans = set()
+  global x,y
+  for dir in dirs:
+    nx = x + keyset[dir][0]
+    ny = y + keyset[dir][1]
+    if not checkLocation(nx,ny):
+      continue
+    
+    ans.add((x,y,nx,ny))
+    ans.add((nx,ny,x,y))
+    x, y = nx, ny
+
+  return len(ans)/2
+
+def checkLocation(nx,ny):
+  return 0 <= nx < 11 and 0 <= ny < 11
+
+# dirs = 'ULURRDLLU'
+dirs = 'ULURRDLLU'
+print(solution_7(dirs))
---
+++ step6-10.py
@@ -0,0 +1,48 @@
+# 6 실패율
+def solution_6(N, stages):
+    ret = [0] * N
+    total = len(stages)
+    
+    for i in range(1, N + 1):  # 스테이지 번호는 1부터 시작
+        stage_total = total
+        fail_count = 0
+
+        # 실패한 스테이지 계산
+        fail_count = stages.count(i)
+        print('i', i, 'fail_count : ', fail_count, ' stage_total : ', stage_total)
+        # 현재 스테이지에 도달한 플레이어 수
+        if stage_total > 0:
+            ret[i - 1] = fail_count / stage_total
+        else:
+            ret[i - 1] = 0  # 도달한 플레이어가 없으면 실패율 0
+        
+        # 현재 스테이지에 있는 플레이어 제외 (다음 스테이지로)
+        total -= fail_count
+
+    return ret
+
+# 7 방문길이
+keyset = { 'U' : (0,-1), 'D' : (0,1), 'L' : (-1,0), 'R' : (1,0) }
+x,y = 5,5
+
+def solution_7(dirs):
+  ans = set()
+  global x,y
+  for dir in dirs:
+    nx = x + keyset[dir][0]
+    ny = y + keyset[dir][1]
+    if not checkLocation(nx,ny):
+      continue
+    
+    ans.add((x,y,nx,ny))
+    ans.add((nx,ny,x,y))
+    x, y = nx, ny
+
+  return len(ans)/2
+
+def checkLocation(nx,ny):
+  return 0 <= nx < 11 and 0 <= ny < 11
+
+# dirs = 'ULURRDLLU'
+dirs = 'ULURRDLLU'
+print(solution_7(dirs))
