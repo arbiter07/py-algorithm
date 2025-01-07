4f78b9a1c9b345d54cea10bfd85f90754887f490
arbiter <arbiter08@naver.com>
2025-01-07 19:46:59 +0900

    # 13

--- ing_problem.py
+++ ing_problem.py
@@ -1,5 +1,9 @@
-# 12 주식가격 
-def solution_12(s): 
-  return 0
+# 13 크레인 인형 뽑기 
+def solution_13(board, moves): 
+  ret = 0
+  
+  return ret
 
-print(solution_12(s="0"))
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+print(solution_13(board, moves))
--- step11-15.py
+++ step11-15.py
@@ -12,3 +12,17 @@ def solution_11(s):
   return (not stack) * 1
 
 print(solution_11(s="baabaa"))
+
+# 12 주식가격 
+def solution_12(s): 
+  ret = []
+  for i, c in enumerate(s):
+    cnt = 0
+    for index in range(i+1,len(s)):
+      cnt += 1
+      if c > s[index]:
+        break
+    ret.append(cnt)
+  return ret
+
+print(solution_12(s=[1,2,3,2,3])) # [4,3,1,1,0]
