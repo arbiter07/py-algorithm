0d28754881e5443aa4aca6b09d287658e0c9adba
arbiter <arbiter08@naver.com>
2025-01-08 01:40:40 +0900

    # 14

--- ing_problem.py
+++ ing_problem.py
@@ -1,9 +1,7 @@
-# 13 크레인 인형 뽑기 
-def solution_13(board, moves): 
+# 14 표 편집
+def solution_14(): 
   ret = 0
-  
   return ret
+           
 
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
-print(solution_13(board, moves))
+print(solution_14())
--- step11-15.py
+++ step11-15.py
@@ -26,3 +26,38 @@ def solution_12(s):
   return ret
 
 print(solution_12(s=[1,2,3,2,3])) # [4,3,1,1,0]
+
+# 13 크레인 인형 뽑기 
+def solution_13(board, moves): 
+  ret = 0
+  lanes = [[] for _ in range(len(board[0]))]
+  for i in range(len(board) - 1 , -1 , -1):
+    for j in range(len(board[0])):
+      if board[i][j]:
+        lanes[j].append(board[i][j])
+  
+  bucket = []
+  for move in moves:
+    lane = lanes[move-1]
+    if lane:
+      popValue = lane.pop()
+      if not bucket:
+        bucket.append(popValue)
+      else:
+        if bucket[-1] == popValue:
+          bucket.pop()
+          ret += 2
+        else:
+          bucket.append(popValue)
+  return ret
+           
+
+board = [
+           [0,0,0,0,0]
+          ,[0,0,1,0,3]
+          ,[0,2,5,0,1]
+          ,[4,2,4,4,2]
+          ,[3,5,1,3,1]
+        ]
+moves = [1,5,3,5,1,2,1,4]
+print(solution_13(board, moves))
