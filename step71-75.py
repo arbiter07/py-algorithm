92eb652b56ac88704851a6850a667e1265c86160
arbiter <arbiter08@naver.com>
2025-02-03 14:09:48 +0900

    # 76

--- step71-75.py
+++ step71-75.py
@@ -62,3 +62,29 @@ def solution_74(n):
 
 n = 4
 print(solution_74(n))
+
+# 75 정수삼각형
+def solution_75(triangle : list):
+  result = 0
+  
+  dp = [[ 0 for _ in range(len(triangle)) ] for _ in range(len(triangle)) ]
+
+  dp[0][0] = triangle[0][0]
+  dp[1][0] = triangle[1][0] + dp[0][0]
+  dp[1][1] = triangle[1][1] + dp[0][0]
+  
+
+  for i in range(2, len(triangle)): # 2 3 4
+    for j in range(len(triangle[i])): # 1 2 3 4 5
+      if j == 0:
+        dp[i][j] += dp[i-1][j] + triangle[i][j]
+      elif j == len(triangle[i]) - 1:
+        dp[i][j] += dp[i-1][j-1] + triangle[i][j]
+      else:
+        dp[i][j] += max(dp[i-1][j-1],dp[i-1][j]) + triangle[i][j]
+
+
+  return max(dp[len(triangle)-1])
+
+triangle = [ [7],[3,8],[8,1,0],[2,7,4,4],[4,5,2,6,5] ]
+print(solution_75(triangle)) # 30
